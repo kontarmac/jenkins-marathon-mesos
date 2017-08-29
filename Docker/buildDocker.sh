c8e1b5b1a6f936c808b323bf1bb8dcfd096e3893
kontarmac <carlos.gonz.cantalapiedra@gmail.com>
2017-08-29 15:50:16 +0200

    Shell script to create docker image and push it to docker hub

--- Docker/buildDocker.sh
+++ Docker/buildDocker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 docker login -u carlosgonzalezcantalapiedra -p $1
-docker build -f carlosgonzalezcantalapiedra/jenkins-marathon-mesos:$2 .
+docker build -t carlosgonzalezcantalapiedra/jenkins-marathon-mesos:$2 .
 docker push carlosgonzalezcantalapiedra/jenkins-marathon-mesos:$2
